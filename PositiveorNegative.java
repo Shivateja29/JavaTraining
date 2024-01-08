7d25e120f15606558c21e5c86e110bfa75cbf9ad
Shivateja29 <155037529+Shivateja29@users.noreply.github.com>
2024-01-07 22:38:24 -0500

    Add files via upload

---
+++ PositiveorNegative.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PositiveorNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc =new Scanner(System.in);
+		 System.out.println("Enter number :");
+		 int number = sc.nextInt();
+		 if (number>=0)
+		 {
+			 System.out.println("Number is positive");
+		 }else
+		 {
+			 System.out.println("Number is negative");
+		 }
+		 
+	}
+
+}
